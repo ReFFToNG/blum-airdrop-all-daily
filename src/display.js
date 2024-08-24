a3aa8460fb6aea6ff93a9d494aa50f3b66736d61
ReFFToNG <49006315+ReFFToNG@users.noreply.github.com>
2024-08-24 14:07:15 +0700

    Add files via upload

---
+++ src/display.js
@@ -0,0 +1,11 @@
+function displayHeader() {
+  process.stdout.write('\x1Bc');
+  console.log('==================================================='.cyan);
+  console.log('=            🌟 Blum Airdrop Bot 🌟               ='.cyan);
+  console.log('=               Created Reffi.Ron                 ='.cyan);
+  console.log('=        https://www.tiktok.com/@reffi_kusuma     ='.cyan);
+  console.log('==================================================='.cyan);
+  console.log();
+}
+
+module.exports = { displayHeader };
